8c46256d9c0da0a476d6993f01415884ea6ebd64
Customer <u25-oe15ihoobadd@c109366.sgvps.net>
2023-05-10 13:07:55 +0000

    Correccions tailwind

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="/css/app.css" rel="stylesheet">
+    <link href="{{ asset('css/output.css') }}" rel="stylesheet">
 </head>
