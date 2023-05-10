e02ab3ff6a34604b0f815a738dfdc77d9820e170
Customer <u25-oe15ihoobadd@c109366.sgvps.net>
2023-05-10 11:09:25 +0000

    Borrat tailwind

--- laravel/resources/views/login.blade.php
+++ laravel/resources/views/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('css/output.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
 <body>
