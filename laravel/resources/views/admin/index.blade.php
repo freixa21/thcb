7801249fdc0bfc3d479d8adc372eeae99f8d6974
Max Freixa <freixa.sureda.max@alumnat.copernic.cat>
2023-05-22 10:36:48 +0200

    Millores vista admin index

--- laravel/resources/views/admin/index.blade.php
+++ laravel/resources/views/admin/index.blade.php
@@ -5,135 +5,135 @@
 @section('content')
     <div class="flex flex-col w-full">
         <div id="wrapper-general" class="flex flex-col lg:flex-row w-full lg:pl-24 lg:pr-24 pl-3 pr-3">
-            <div class="col-admin w-full bg-blue-400">
+            <div class="col-admin lg:w-full bg-white rounded-md m-3 px-2 py-2 shadow-md">
                 <h2>General</h2>
                 <table>
                     <tr>
                         <td>Inscrits totals:</td>
-                        <td> {{ $totalInscrits }}</td>
+                        <td class="text-right"> {{ $totalInscrits }}</td>
                     </tr>
                     <tr>
                         <td>Inscrits confirmats:</td>
-                        <td> {{ $totalIngressosConfirmats }}</td>
+                        <td class="text-right"> {{ $totalIngressosConfirmats }}</td>
                     </tr>
                     <tr>
                         <td>Afters totals:</td>
-                        <td> {{ $totalAfters }}</td>
+                        <td class="text-right"> {{ $totalAfters }}</td>
                     </tr>
                     <tr>
                         <td>Homes:</td>
-                        <td>{{ $totalHomes }}</td>
+                        <td class="text-right"> {{ $totalHomes }}</td>
                     </tr>
                     <tr>
                         <td>Dones:</td>
-                        <td>{{ $totalDones }}</td>
+                        <td class="text-right"> {{ $totalDones }}</td>
                     </tr>
                     <tr>
                         <td>Ingressos previstos</td>
-                        <td>{{ $totalIngressosPrevistos }}</td>
+                        <td class="text-right"> {{ $totalIngressosPrevistos }}€</td>
                     </tr>
                     <tr>
                         <td>Ingressos confirmats:</td>
-                        <td>{{ $totalIngressosConfirmats }}</td>
+                        <td class="text-right"> {{ $totalIngressosConfirmats }}€</td>
                     </tr>
                 </table>
             </div>
-            <div class="col-admin lg:w-full bg-blue-400">
+            <div class="col-admin lg:w-full bg-white rounded-md m-3 px-2 py-2 shadow-md">
                 <h2>Equips</h2>
                 <table>
                     <tr>
                         <td>Equips inscrits: </td>
-                        <td>{{ $totalEquips }}</td>
+                        <td class="text-right"> {{ $totalEquips }}</td>
                     </tr>
                     <tr>
                         <td>Equips confirmats:</td>
-                        <td>{{ $equipsConfirmats }}</td>
+                        <td class="text-right"> {{ $equipsConfirmats }}</td>
                     </tr>
                     <tr>
                         <td>Jugadors inscrits:</td>
-                        <td>{{ $totalJugadors }}</td>
+                        <td class="text-right"> {{ $totalJugadors }}</td>
                     </tr>
                     <tr>
                         <td>Jugadors confirmats:</td>
-                        <td>{{ $jugadorsConfirmats }}</td>
+                        <td class="text-right"> {{ $jugadorsConfirmats }}</td>
                     </tr>
                     <tr>
                         <td>Afters:</td>
-                        <td>{{ $afterJugadors }}</td>
+                        <td class="text-right"> {{ $afterJugadors }}</td>
                     </tr>
                     <tr>
                         <td>Ingressos previstos:</td>
-                        <td>{{ $ingressosPrevistosJugadors }}</td>
+                        <td class="text-right"> {{ $ingressosPrevistosJugadors }}€</td>
                     </tr>
                     <tr>
                         <td>Ingressos confirmats:</td>
-                        <td>{{ $ingressosConfirmatsJugadors }}</td>
+                        <td class="text-right"> {{ $ingressosConfirmatsJugadors }}€</td>
                     </tr>
                 </table>
             </div>
-            <div class="col-admin lg:w-full bg-blue-400">
+            <div class="col-admin lg:w-full bg-white rounded-md m-3 px-2 py-2 shadow-md">
                 <h2>Espectadors</h3>
                     <table>
                         <tr>
                             <td>Espectadors inscrits: </td>
-                            <td>{{ $espectadorsInscrits }}</td>
+                            <td class="text-right"> {{ $espectadorsInscrits }}</td>
                         </tr>
                         <tr>
                             <td>Espectadors confirmats:</td>
-                            <td>{{ $espectadorsConfirmats }}</td>
+                            <td class="text-right"> {{ $espectadorsConfirmats }}</td>
                         </tr>
                         <tr>
                             <td>Afters:</td>
-                            <td>{{ $afterEspectadors }}</td>
+                            <td class="text-right"> {{ $afterEspectadors }}</td>
                         </tr>
                         <tr>
                             <td>Ingressos previstos:</td>
-                            <td>{{ $ingressosPrevistosEspectadors }}</td>
+                            <td class="text-right"> {{ $ingressosPrevistosEspectadors }}€</td>
                         </tr>
                         <tr>
                             <td>Ingressos confirmats:</td>
-                            <td>{{ $ingressosConfirmatsEspectadors }}</td>
+                            <td class="text-right"> {{ $ingressosConfirmatsEspectadors }}€</td>
                         </tr>
                     </table>
             </div>
         </div>
         <div class="flex flex-col lg:flex-row w-full lg:pl-24 lg:pr-24 pl-3 pr-3 mt-5">
-            <div class="col-admin lg:w-full bg-blue-400">
+            <div class="col-admin lg:w-full bg-white rounded-md m-3 px-2 py-2 shadow-md">
                 <h2>Samarretes</h2>
                 <table>
                     <tr>
                         <td>S: </td>
-                        <td>{{ $totalS }}</td>
+                        <td class="text-right"> {{ $totalS }}</td>
                     </tr>
                     <tr>
                         <td>M:</td>
-                        <td>{{ $totalM }}</td>
+                        <td class="text-right"> {{ $totalM }}</td>
                     </tr>
                     <tr>
                         <td>L:</td>
-                        <td>{{ $totalL }}</td>
+                        <td class="text-right"> {{ $totalL }}</td>
                     </tr>
                     <tr>
                         <td>XL:</td>
-                        <td>{{ $totalXL }}</td>
+                        <td class="text-right"> {{ $totalXL }}</td>
                     </tr>
                     <tr>
                         <td>XXL:</td>
-                        <td>{{ $totalXXL }}</td>
+                        <td class="text-right"> {{ $totalXXL }}</td>
                     </tr>
                 </table>
             </div>
-            <div class="col-admin lg:w-full bg-blue-400">
+            <div class="col-admin lg:w-full bg-white rounded-md m-3 px-2 py-2 shadow-md">
                 <h2>Al·lèrgies</h2>
                 <table>
                     <tr class="text-left">
                         <th>Nom</th>
-                        <th>Al·lèrgies</th>
+                        <th class="text-right">Al·lèrgies</th>
                     </tr>
                     @foreach ($alergias as $item)
                         <tr>
-                            <td>{{ $item['nombre'] }} {{ $item['apellidos'] }}</td>
-                            <td>{{ $item['alergia'] }}</td>
+                            <td> {{ $item['nombre'] }} {{ $item['apellidos'] }}</td>
+                            <td class="text-right"> {{ $item['alergia'] }}</td>
                         </tr>
                     @endforeach
                 </table>
