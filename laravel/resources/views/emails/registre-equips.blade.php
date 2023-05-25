d6df61c064eb69b793fa739bc0c4a6bf1f5f7400
Max Freixa <freixa.sureda.max@alumnat.copernic.cat>
2023-05-25 14:56:54 +0200

    Canvi missatge equip registrat

--- laravel/resources/views/emails/registre-equips.blade.php
+++ laravel/resources/views/emails/registre-equips.blade.php
@@ -1,8 +1,8 @@
 @component('mail::message')
-# T'has registrat correctament!
+# Equip registrat correctament!
 
-Recorda que per completar la inscripció s'han de posar tots els jugadors del teu equip i fer el pagament.
-Un cop facis el pagament i ens enviïs el comprovant a través de la web, t'enviarem un correu electrònic confirmant la teva inscripció.
+Recorda que per completar la inscripció s'han de posar tots els jugadors del teu equip i fer el pagament.<br>
+Un cop facis el pagament i ens enviïs el comprovant a través de la web, us enviarem un correu electrònic confirmant la teva inscripció.
 
 @component('mail::button', ['url' => route('auth.login')])
 Iniciar sessió
