787fd2d7d256c26ba0d8019b19ed6780d2a91c5a
Max Freixa <freixa.sureda.max@alumnat.copernic.cat>
2023-05-30 15:12:57 +0200

    Millora instruccions per mail

--- laravel/resources/views/emails/registre-equips.blade.php
+++ laravel/resources/views/emails/registre-equips.blade.php
@@ -4,6 +4,14 @@
 Recorda que per completar la inscripció s'han de posar tots els jugadors del teu equip i fer el pagament.<br>
 Un cop facis el pagament i ens enviïs el comprovant a través de la web, us enviarem un correu electrònic confirmant la vostra inscripció.
 
+<strong>Passos a seguir:</strong>
+1. Inscriu al teu equip un mínim de 5 jugadors, recorda que tu també t'has d'afegir.
+2. Un cop hagis apuntat a tots els jugadors, podràs veure l'import total de la inscripció de l'equip per fer el pagament.
+3. Fes el pagament per Verse o Bizum tal com indica les instruccions i fes una captura del rebut/pagament (Les podràs veure quan tinguis la sessió iniciada).
+4. Adjunta la captura de pantalla del rebut i envia-la.
+
+Un cop verifiquem el pagament rebràs un correu i la inscripció del teu equip quedarà confirmada!
+
 @component('mail::button', ['url' => route('auth.login')])
 Iniciar sessió
 @endcomponent
--- laravel/resources/views/emails/registre-espectadors.blade.php
+++ laravel/resources/views/emails/registre-espectadors.blade.php
@@ -2,7 +2,14 @@
 # T'has registrat correctament!
 
 Per completar la inscripció, revisa les dades i realitza el pagament.<br>
-Un cop facis el pagament i ens enviïs el comprovant a través de la web, t'enviarem un correu electrònic confirmant la teva inscripció.
+
+<strong>Passos a seguir:</strong>
+1. Inscriu al teu equip un mínim de 5 jugadors, recorda que tu també t'has d'afegir.
+2. Un cop hagis apuntat a tots els jugadors, podràs veure l'import total de la inscripció de l'equip per fer el pagament.
+3. Fes el pagament per Verse o Bizum tal com indica les instruccions i fes una captura del rebut/pagament (Les podràs veure quan tinguis la sessió iniciada).
+4. Adjunta la captura de pantalla del rebut i envia-la.
+
+Un cop verifiquem el pagament rebràs un correu i la teva inscripció quedarà confirmada!
 
 @component('mail::button', ['url' => route('auth.login')])
 Iniciar sessió
