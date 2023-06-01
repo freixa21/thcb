ee29c255da00689877a17021447e6cbe06912295
Max Freixa <freixa.sureda.max@alumnat.copernic.cat>
2023-06-01 15:59:06 +0200

    Logo email + estil mail, colors

--- laravel/resources/views/vendor/mail/html/header.blade.php
+++ laravel/resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,8 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="{{asset('images/beach-hockey-logo-blau.png')}}" class="logo" alt="Beach Hockey">
+@if (trim($slot) === 'Torneig Hockey Costa Brava')
+<img src="https://hockeycostabrava.com/wp-content/uploads/2023/05/beach23_dark.png" class="logo" alt="Beach Hockey">
 @else
 {{ $slot }}
 @endif
