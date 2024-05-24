62b6266f42b10192de3cc044c2583f0915cc87ff
Max Freixa <freixa.sureda.max@alumnat.copernic.cat>
2024-05-24 14:26:56 +0200

    mail espectador fix

--- laravel/resources/views/emails/registre-espectadors.blade.php
+++ laravel/resources/views/emails/registre-espectadors.blade.php
@@ -1,13 +1,12 @@
 @component('mail::message')
 # T'has registrat correctament!
 
-Per completar la inscripció, revisa les dades i realitza el pagament.<br>
+Per completar la inscripció com a espectador, revisa les dades i realitza el pagament.<br>
 
 <strong>Passos a seguir:</strong>
-1. Inscriu al teu equip un mínim de 5 jugadors, recorda que tu també t'has d'afegir.
-2. Un cop hagis apuntat a tots els jugadors, podràs veure l'import total de la inscripció de l'equip per fer el pagament.
-3. Fes el pagament per Bizum tal com indica les instruccions i fes una captura del rebut/pagament (Les podràs veure quan tinguis la sessió iniciada).
-4. Adjunta la captura de pantalla del rebut i envia-la a través de la web a l'apartat "Estat de la inscripció".
+1. Accedeix al teu perfil per comprovar les teves dades, allà podràs veure el preu de la inscripció
+2. Fes el pagament per Bizum tal com indica les instruccions i fes una captura del rebut/pagament (Les podràs veure quan tinguis la sessió iniciada).
+3. Adjunta la captura de pantalla del rebut i envia-la a través de la web a l'apartat "Estat de la inscripció".
 
 Un cop verifiquem el pagament rebràs un correu i la teva inscripció quedarà confirmada!
 
