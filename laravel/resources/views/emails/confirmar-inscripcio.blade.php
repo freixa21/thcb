9a410b9ad659fbd2aad53e9e33f6b104aefea122
Max Freixa <freixa.sureda.max@alumnat.copernic.cat>
2024-06-02 15:12:48 +0200

    Canvi email view

--- laravel/resources/views/emails/confirmar-inscripcio.blade.php
+++ laravel/resources/views/emails/confirmar-inscripcio.blade.php
@@ -3,7 +3,7 @@
 
 La teva inscripció al Torneig Hockey Costa Brava 2024 ha quedat confirmada.
 
-Ens veiem el pròxim dissabte 29 de Juliol a Platja d'Aro!
+Ens veiem el pròxim dissabte 27 de Juliol a Platja d'Aro!
 
 Estigueu atents al nostre instagram per no perde-us cap novetat.
 
